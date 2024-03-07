261a09c4620cdc2a1d4e03db17e70a3a5c6b09cd
Serge-boro <serhiiborodin83@gmail.com>
2024-03-06 20:14:57 -0500

    add changes

--- functions/products.js
+++ functions/products.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const Airtable = require('airtable-node')
-const airtable = new Airtable({
+
+Airtable.configure({
   apiKey:
     'pateCt2hAGMBMCxRx.cd7a4ad9d72c6f3ce7244d502b16c2c4ff61ffc267dd46a4b506ecbee245c4b0',
 })
-  .base('app9M53VVRehypn3c')
-  .table('products')
+const airtable = new Airtable().base('app9M53VVRehypn3c').table('products')
 
 exports.handler = async (event, context) => {
   const { id } = event.queryStringParameters
